206cbd2643a024b8a6aea772a642d7df7b73b5bd
ytorii <metalwarrior_type_jp@hotmail.com>
2019-09-07 23:55:16 +0900

    Refactor Answer2 to more ideomatic rust code.

--- src/question_2.rs
+++ src/question_2.rs
@@ -1,23 +1,28 @@
 use regex::Regex;
 use std::path::Path;
 
-use image::GenericImageView;
+use image::{GenericImageView, GrayImage};
 
 pub fn answer(file: &str) {
     let im = image::open(&Path::new(&file)).unwrap();
-    let mut gray_imgbuf = image::GrayImage::new(im.width(), im.height());
 
-    gray_imgbuf
-        .enumerate_pixels_mut()
-        .for_each(|(x, y, pixel)| {
-            let rgb = im.get_pixel(x, y);
-            pixel[0] = (0.2126 * (rgb[0] as f32)
-                + 0.7152 * (rgb[1] as f32)
-                + 0.0722 * (rgb[2] as f32)) as u8;
-        });
+    let gray_imgbuf = im.to_rgb().enumerate_pixels().fold(
+        GrayImage::new(im.width(), im.height()),
+        |mut res, (x, y, pixel)| {
+            res.put_pixel(x, y, gray_pixel_of(pixel));
+            res
+        },
+    );
 
     let re = Regex::new(r"\.jpg").unwrap();
-    let out_filename = format!("{}_gray.jpg", re.replace_all(&file, ""));
+    let out_filename = format!("{}", re.replace_all(&file, "_gray.jpg"));
 
     gray_imgbuf.save(Path::new(&out_filename)).unwrap();
+}
+
+fn gray_pixel_of(rgb_pixel: &image::Rgb<u8>) -> image::Luma<u8> {
+    let gray_pixel = (0.2126 * (rgb_pixel[0] as f32)
+        + 0.7152 * (rgb_pixel[1] as f32)
+        + 0.0722 * (rgb_pixel[2] as f32)) as u8;
+    image::Luma([gray_pixel])
 }
