51bea90b42dfac8412f4131365c4bc3e95fb5c8c
ytorii <metalwarrior_type_jp@hotmail.com>
2019-09-11 00:56:17 +0900

    Refactor Answer4

--- src/question_4.rs
+++ src/question_4.rs
@@ -2,15 +2,14 @@ use image::{DynamicImage, ImageBuffer, Luma};
 
 pub fn answer(im: &DynamicImage) -> ImageBuffer<Luma<u8>, Vec<u8>> {
     let mut gray_buf = im.to_luma();
-    let mut t = 1;
-    let mut sb_max = 1.0;
 
-    (1..255).for_each(|i| {
-        let sb_i = sb(i as u8, &gray_buf);
-        if sb_i > sb_max {
-            sb_max = sb_i;
-            t = i;
+    let (t, _sb_max) = (1..255).fold((0, 0.0), |mut res, i| {
+        let sb_i = sb(i, &gray_buf);
+        if sb_i > res.1 {
+            res.0 = i;
+            res.1 = sb_i;
         }
+        res
     });
 
     gray_buf.pixels_mut().for_each(|pixel| {
@@ -25,20 +24,24 @@ pub fn answer(im: &DynamicImage) -> ImageBuffer<Luma<u8>, Vec<u8>> {
 }
 
 fn sb(t: u8, buf: &ImageBuffer<Luma<u8>, Vec<u8>>) -> f32 {
-    let vec0: Vec<f32> = buf
-        .pixels()
-        .filter(|pixel| pixel[0] < t)
-        .map(|luma| luma[0] as f32)
-        .collect();
-    let vec1: Vec<f32> = buf
-        .pixels()
-        .filter(|pixel| t <= pixel[0])
-        .map(|luma| luma[0] as f32)
-        .collect();
-    let w0 = (vec0.len() as f32) / (buf.pixels().len() as f32);
+    let (class0, class1) = classes_of(t, buf);
 
-    let m0 = (vec0.iter().sum::<f32>()) / (vec0.len() as f32);
-    let m1 = (vec1.iter().sum::<f32>()) / (vec1.len() as f32);
+    let w0 = (class0.len() as f32) / (buf.pixels().len() as f32);
+
+    let m0 = (class0.iter().sum::<f32>()) / (class0.len() as f32);
+    let m1 = (class1.iter().sum::<f32>()) / (class1.len() as f32);
 
     w0 * (1.0 - w0) * (m0 - m1).powi(2)
+}
+
+fn classes_of(t: u8, buf: &ImageBuffer<Luma<u8>, Vec<u8>>) -> (Vec<f32>, Vec<f32>) {
+    buf.pixels().fold((vec![], vec![]), |mut res, &pixel| {
+        let Luma([p]) = pixel;
+        if p < t {
+            res.0.push(p as f32)
+        } else {
+            res.1.push(p as f32)
+        }
+        res
+    })
 }
