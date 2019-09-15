8a5077913ddf3dbf054f001e3fad1ebf1c6e5551
ytorii <metalwarrior_type_jp@hotmail.com>
2019-09-15 11:36:54 +0900

    Refactor Q5 with struct

--- src/question_5.rs
+++ src/question_5.rs
@@ -3,60 +3,73 @@ use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb, RgbImage};
 pub fn answer(im: &DynamicImage) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let image_buf = im.to_rgb();
 
-    let hsvs = rgb_to_inverted_hsv(&image_buf);
-    hsv_to_rgb(hsvs, im.width(), im.height())
+    let hsvs = rgb_to_hsvs(&image_buf);
+    hsv_to_inverted_rgb(hsvs, im.width(), im.height())
 }
 
-fn rgb_to_inverted_hsv(buf: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> Vec<(i32, i32, i32)> {
-    buf.pixels().fold(vec![], |mut acc, pixel| {
-        let Rgb(rgb) = pixel;
-
-        let (max, min) = (rgb.iter().max().unwrap(), rgb.iter().min().unwrap());
-
-        let s = (max - min) as f32;
-
-        let h: i32 = match (max, min) {
-            (x, y) if x == y => 180 % 360,
-            (_x, y) if rgb[2] == *y => {
-                ((60.0 * (rgb[1] as f32 - rgb[0] as f32) / s + 60.0) as i32 + 180) % 360
-            }
-            (_x, y) if rgb[0] == *y => {
-                ((60.0 * (rgb[2] as f32 - rgb[1] as f32) / s + 180.0) as i32 + 180) % 360
-            }
-            _ => ((60.0 * (rgb[0] as f32 - rgb[2] as f32) / s + 300.0) as i32 + 180) % 360,
-        };
-
-        acc.push((h, s as i32, *max as i32));
+fn rgb_to_hsvs(buf: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> Vec<Hsv> {
+    buf.pixels().fold(vec![], |mut acc, rgb_pixel| {
+        let hsv = Hsv::from_rgb(rgb_pixel);
+        acc.push(hsv);
         acc
     })
 }
 
-fn hsv_to_rgb(
-    hsvs: Vec<(i32, i32, i32)>,
-    width: u32,
-    height: u32,
-) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn hsv_to_inverted_rgb(hsvs: Vec<Hsv>, width: u32, height: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     hsvs.iter()
         .enumerate()
-        .fold(RgbImage::new(width, height), |mut res, (i, &(h, s, v))| {
-            let c = s as f32;
-            let hd = h as f32 / 60.0;
-            let x = c * (1.0 - (hd % 2.0 - 1.0).abs());
-            let d = v as f32 - c;
-            let (r, g, b) = match hd {
-                y if 0.0 <= y && y < 1.0 => ((c + d), (x + d), d),
-                y if 1.0 <= y && y < 2.0 => ((x + d), (c + d), d),
-                y if 2.0 <= y && y < 3.0 => (d, (c + d), (x + d)),
-                y if 3.0 <= y && y < 4.0 => (d, (x + d), (c + d)),
-                y if 4.0 <= y && y < 5.0 => ((x + d), d, (c + d)),
-                y if 5.0 <= y && y < 6.0 => ((c + d), d, (x + d)),
-                _ => (d, d, d),
-            };
-            res.put_pixel(
-                i as u32 % width,
-                i as u32 / width,
-                Rgb([r.round() as u8, g.round() as u8, b.round() as u8]),
-            );
+        .fold(RgbImage::new(width, height), |mut res, (i, hsv)| {
+            res.put_pixel(i as u32 % width, i as u32 / width, hsv.invert().to_rgb());
             res
         })
+}
+
+struct Hsv {
+    h: f32,
+    s: f32,
+    v: f32,
+}
+
+impl Hsv {
+    pub fn from_rgb(rgb_pixel: &Rgb<u8>) -> Hsv {
+        let Rgb(rgb) = rgb_pixel;
+
+        let (max, min) = (rgb.iter().max().unwrap(), rgb.iter().min().unwrap());
+        let v = *max as f32;
+        let s = v - *min as f32;
+        let h: f32 = match (max, min) {
+            (x, y) if x == y => 180.0 % 360.0,
+            (_x, y) if rgb[2] == *y => 60.0 * (rgb[1] as f32 - rgb[0] as f32) / s + 60.0,
+            (_x, y) if rgb[0] == *y => 60.0 * (rgb[2] as f32 - rgb[1] as f32) / s + 180.0,
+            _ => 60.0 * (rgb[0] as f32 - rgb[2] as f32) / s + 300.0,
+        };
+        Hsv { h: h, s: s, v: v }
+    }
+
+    fn invert(&self) -> Hsv {
+        Hsv {
+            h: (self.h + 180.0) % 360.0,
+            s: self.s,
+            v: self.v,
+        }
+    }
+
+    fn to_rgb(&self) -> Rgb<u8> {
+        let c = self.s;
+        let hd = self.h / 60.0;
+        let x = c * (1.0 - (hd % 2.0 - 1.0).abs());
+        let d = self.v - c;
+
+        let (r, g, b) = match hd {
+            y if 0.0 <= y && y < 1.0 => (c, x, 0.0),
+            y if 1.0 <= y && y < 2.0 => (x, c, 0.0),
+            y if 2.0 <= y && y < 3.0 => (0.0, c, x),
+            y if 3.0 <= y && y < 4.0 => (0.0, x, c),
+            y if 4.0 <= y && y < 5.0 => (x, 0.0, c),
+            y if 5.0 <= y && y < 6.0 => (c, 0.0, x),
+            _ => (0.0, 0.0, 0.0),
+        };
+
+        Rgb([(r + d) as u8, (g + d) as u8, (b + d) as u8])
+    }
 }
